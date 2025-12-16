d44f2618b6534757d74379a25974a6b88d157ff4
sakezerto <88200289+sakezerto@users.noreply.github.com>
2025-12-16 09:26:05 +0800

    Add PROCESS-NAME to MAP_DICT in main.py

--- main.py
+++ main.py
@@ -13,7 +13,8 @@ MAP_DICT = {'DOMAIN-SUFFIX': 'domain_suffix', 'HOST-SUFFIX': 'domain_suffix', 'h
             'DOMAIN-KEYWORD':'domain_keyword', 'HOST-KEYWORD': 'domain_keyword', 'host-keyword': 'domain_keyword', 'IP-CIDR': 'ip_cidr',
             'ip-cidr': 'ip_cidr', 'IP-CIDR6': 'ip_cidr', 
             'IP6-CIDR': 'ip_cidr','SRC-IP-CIDR': 'source_ip_cidr', 'GEOIP': 'geoip', 'DST-PORT': 'port',
-            'SRC-PORT': 'source_port', "URL-REGEX": "domain_regex", "DOMAIN-REGEX": "domain_regex"}
+            'SRC-PORT': 'source_port', "URL-REGEX": "domain_regex", "DOMAIN-REGEX": "domain_regex", 'PROCESS-NAME': 'process_name',
+    'process-name': 'process_name'}
 
 def read_yaml_from_url(url):
     headers = {'User-Agent': 'Mozilla/5.0'}
